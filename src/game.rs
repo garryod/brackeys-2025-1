d9b0c0998dc00e4ef26fc45fc8dbcdda1c336d65
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2025-02-16 10:31:34 +0000

    setup leafwing input manageer

--- src/game.rs
+++ src/game.rs
@@ -2,13 +2,16 @@ use crate::{cleanup, fps_counter::FpsPlugin, AppState};
 use bevy::{
     app::Plugin,
     prelude::{Camera3d, Commands, Component, OnEnter, OnExit},
+    reflect::Reflect,
 };
+use leafwing_input_manager::{plugin::InputManagerPlugin, Actionlike};
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_plugins(FpsPlugin::default())
+            .add_plugins(InputManagerPlugin::<Action>::default())
             .add_systems(OnEnter(AppState::Game), setup)
             .add_systems(OnExit(AppState::Game), cleanup::<Game>);
     }
@@ -16,6 +19,9 @@ impl Plugin for GamePlugin {
 
 #[derive(Component)]
 struct Game;
+
+#[derive(Debug, Actionlike, Clone, Copy, PartialEq, Eq, Hash, Reflect)]
+enum Action {}
 
 fn setup(mut commands: Commands) {
     commands.spawn((Game, Camera3d::default()));
